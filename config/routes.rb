bd554613a9caad620f7aa40c38e7423fc38c4845
github username <github email address>
2017-11-16 09:20:49 -0500

    created home route and homepage view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :products
   resources :categories
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'static#home', as: 'home_path'
+  root to: 'static#home'
+
 end
