1397589bc7001f58944d6b6db383bbacb257444e
evaccaro <erv230@nyu.edu>
2017-11-16 17:04:32 -0500

    dont use these changes please

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,17 @@ class UsersController < ApplicationController
 
 def create
   @user = User.new(user_params)
-  if @user.save
-    session[:id] = @user.id
-    redirect_to user_path(@user)
-  else
-    flash[:notice] = "That is not a valid user."  # can i iterate through error messages and display each one as a flash notice?
-    render :new
+  if @user.categories.length == 3
+    if @user.save
+      session[:id] = @user.id
+      redirect_to user_path(@user)
+    elsif @user.categories.length != 3
+      flash[:notice] = "Please select 3 categories."
+      render :new
+    else
+      flash[:notice] = "That is not a valid user."  # can i iterate through error messages and display each one as a flash notice?
+      render :new
+    end
   end
 end
 
