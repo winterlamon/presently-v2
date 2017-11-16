b2b39fb67cd82c366758a72fbfc531128db1cab3
github username <github email address>
2017-11-16 12:59:10 -0500

    added listing_url to product table

--- db/migrate/20171115230624_create_products.rb
+++ db/migrate/20171115230624_create_products.rb
@@ -5,6 +5,7 @@ class CreateProducts < ActiveRecord::Migration[5.1]
       t.text :description
       t.float :price
       t.string :image_url
+      t.string :listing_url
       t.integer :category_id
       t.timestamps
     end
