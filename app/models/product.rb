fe5e479488338acba0512e70a3cf6e335cc34ac6
github username <github email address>
2017-11-16 19:50:26 -0500

    no major updates

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,11 +6,23 @@ class Product < ApplicationRecord
       category = Category.find(search)
       if category
         self.where(category_id: category.id)
+          if price
       else
         Product.all
       end
     else
       Product.all
+    end
+  end
+
+  def dollar_range(price)
+    # remvoe $ split string
+
+    if price = "$0-25"
+    elsif price = "26-50"
+    elsif price = "51-75"
+    elsif price = "76-100"
+    end
     end
   end
 end
